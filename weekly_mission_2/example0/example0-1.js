7b9a137d985ce27bce81ca41851ec1b974846462
Ara <ara@pop-os.localdomain>
2022-04-15 18:56:16 -0500

    Ejemplos 1

--- weekly_mission_2/example0/example0-1.js
+++
@@ -1,6 +0,0 @@
-console.log("Objetos")
-
-// Ejemplo 1: Crear un objeto
-const myObjetc = {} // Esto es un objeto vacío
-console.log("Ejemplo 1: Crear un objeto vacío")
-console.log(myObjetc)
--- weekly_mission_2/example0/example0-2.js
+++
@@ -1,9 +0,0 @@
-console.log("Objetos")
-
-// Ejemplo 2: Crear un objeto con propiedades
-const myObjetc2 = {
-    name: "Carlo",
-    age: 27,
-  }
-  console.log("Ejemplo 2: Crear un objeto con propiedades")
-  console.log(myObjetc2)
